c4322fd780a28f986c9342202fa2dd93d2d46c61
ISNing <a1208733578@qq.com>
2022-07-26 12:12:00 +0800

    treble: add kscope multi product kind support

--- generate.sh
+++ generate.sh
@@ -1,50 +1,55 @@
 #!/bin/bash
 
-rom_script=''
-if [ -n "$1" ];then
-	if echo "$1" | grep -qF /;then
-        rom_script=''
-        for i in "$@";do
-            rom_script="$rom_script"$'\n''$(call inherit-product, '$i')'
-        done
-    else
-		rom_script='$(call inherit-product, device/phh/treble/'$1'.mk)'
-	fi
-fi
-
 echo 'PRODUCT_MAKEFILES := \' > AndroidProducts.mk
 
 for part in a ab;do
-	for arch in arm64 arm a64;do
-		extra_packages=""
-                vndk="vndk.mk"
-		optional_base=""
-		if [ "$arch" == "arm" ];then
-			vndk="vndk-binder32.mk"
-		fi
-		if [ "$arch" == "a64" ];then
-			vndk="vndk32.mk"
-		fi
+    for product in common_no_telephony mobile tablet tablet_no_telephony;do
+		for arch in arm64 arm a64;do
+			extra_packages=""
 
-		part_suffix='a'
-		if [ "$part" == 'ab' ];then
-			part_suffix='b'
-		else
-			optional_base='$(call inherit-product, device/phh/treble/base-sas.mk)'
-		fi
-			target="kscope_treble_${arch}_${part_suffix}"
+			rom_script='$(call inherit-product, 'vendor/kscope/target/product/${product}.mk')'
+	                product_suffix=""
+			if [ "$product" == "common_no_telephony" ];then
+				product_suffix="C"
+				model_description="Device with no telephony feature"
+			elif [ "$product" == "mobile" ];then
+				product_suffix="M"
+				model_description="Mobile phone"
+			elif [ "$product" == "tablet" ];then
+				product_suffix="T"
+				model_description="Tablet"
+			elif [ "$product" == "tablet_no_telephony" ];then
+				product_suffix="t"
+				model_description="Tablet with no telephony feature"
+			fi
+			vndk="vndk.mk"
+			optional_base=""
+			if [ "$arch" == "arm" ];then
+				vndk="vndk-binder32.mk"
+			fi
+			if [ "$arch" == "a64" ];then
+				vndk="vndk32.mk"
+			fi
 
-		baseArch="$arch"
-		if [ "$arch" = "a64" ];then
-			baseArch="arm"
-		fi
+			part_suffix='a'
+			if [ "$part" == 'ab' ];then
+				part_suffix='b'
+			else
+				optional_base='$(call inherit-product, device/phh/treble/base-sas.mk)'
+			fi
+				target="kscope_treble_${arch}_${part_suffix}${product_suffix}"
 
-		zygote=32
-		if [ "$arch" = "arm64" ];then
-			zygote=64_32
-		fi
+			baseArch="$arch"
+			if [ "$arch" = "a64" ];then
+				baseArch="arm"
+			fi
 
-		cat > ${target}.mk << EOF
+			zygote=32
+			if [ "$arch" = "arm64" ];then
+				zygote=64_32
+			fi
+
+			cat > ${target}.mk << EOF
 \$(call inherit-product, device/phh/treble/base-pre.mk)
 include build/make/target/product/aosp_${baseArch}.mk
 \$(call inherit-product, device/phh/treble/base.mk)
@@ -55,7 +60,7 @@ PRODUCT_NAME := $target
 PRODUCT_DEVICE := phhgsi_${arch}_$part
 PRODUCT_BRAND := google
 PRODUCT_SYSTEM_BRAND := google
-PRODUCT_MODEL := Phh-Treble
+PRODUCT_MODEL := Phh-Treble for ${model_description}
 
 # Overwrite the inherited "emulator" characteristics
 PRODUCT_CHARACTERISTICS := device
@@ -64,6 +69,7 @@ PRODUCT_PACKAGES += $extra_packages
 
 EOF
 echo -e '\t$(LOCAL_DIR)/'$target.mk '\' >> AndroidProducts.mk
+		done
 	done
 done
 echo >> AndroidProducts.mk
